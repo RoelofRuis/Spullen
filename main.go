9a975910871cc0216892fbec49e641894e8d71f8
RoelofRuis <roelof.ruis@gmail.com>
2020-12-06 21:31:25 +0100

    add objects

--- main.go
+++ main.go
@@ -7,22 +7,38 @@ import (
 	"time"
 )
 
+var o *ObjectList
+
 func main() {
-	o, err := LoadObjectList()
+	objectList, err := LoadObjectList()
 	if err != nil {
 		log.Fatal(err)
 	}
-	obj := &Object{Name: "Stoel", Added: time.Now(), Tags: []string{"bats", "knats"}}
-	o.AddObject(obj)
-	o.Save()
+	o = objectList
+
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/insert", insertHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./static/index.html")
 
-	o, _ := LoadObjectList()
-
 	t.Execute(w, o)
+}
+
+func insertHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	o.AddObject(&Object{
+		Name: r.PostForm.Get("name"),
+		Added: time.Now().Truncate(time.Second),
+	})
+	o.Save()
+
+	http.Redirect(w, r, "/", http.StatusFound)
 }
