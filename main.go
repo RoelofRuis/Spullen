1e00a7423b39057968044c678e4e2d483a5b9a2d
RoelofRuis <roelof.ruis@gmail.com>
2020-12-06 18:02:03 +0100

    layout and adding object add/search bar

--- main.go
+++ main.go
@@ -7,12 +7,11 @@ import (
 
 func main() {
 	http.HandleFunc("/", indexHandler)
-
 	http.ListenAndServe(":8080", nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("./static/index.html") // TODO: move to const
+	t, _ := template.ParseFiles("./static/index.html")
 
 	o, _ := LoadObjectList()
 
