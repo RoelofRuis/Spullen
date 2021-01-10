e1dca3c37937f833b49f528189e293695cec3ede
RoelofRuis <roelof.ruis@gmail.com>
2021-01-10 22:38:26 +0100

    v0.8.0

--- cmd/spullen/main.go
+++ cmd/spullen/main.go
@@ -14,7 +14,7 @@ import (
 	"time"
 )
 
-var VERSION = core.Version{Major: 0, Minor: 7, Patch: 7}
+var VERSION = core.Version{Major: 0, Minor: 8, Patch: 0}
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
