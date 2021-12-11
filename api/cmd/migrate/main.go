fde31cf7524df445ad8d31c7ed50495cbfc945dc
Roelof Ruis <roelof.ruis@gmail.com>
2021-12-11 17:11:09 +0100

    improve migration creation script

--- api/cmd/migrate/main.go
+++ api/cmd/migrate/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/roelofruis/spullen/internal/migration"
 	"log"
+	"os"
 )
 
 func main() {
-	err := migration.Create("structure")
+	var migrationName string
+
+	flag.StringVar(&migrationName, "name", "", "Specify a name for the migration")
+	flag.Parse()
+
+	if migrationName == "" {
+		fmt.Printf("Migration name is required. Set with --name <name>\n")
+		os.Exit(1)
+	}
+
+	err := migration.Create(migrationName)
 	if err != nil {
 		log.Fatal(err)
 	}
